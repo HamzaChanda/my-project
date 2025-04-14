45f8e1b585788e08d9142e94e2f05012412089bd
HamzaChanda <you@example.com>
2025-04-15 01:12:58 +0530

    done

--- app.js
+++ app.js
@@ -56,14 +56,11 @@ const sessionOptions={
     secret:process.env.SECRET,
     resave:false,
     saveUninitialized:true,
-    cookie: {
-      expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',  // Enforce secure cookies on HTTPS
-      sameSite: 'strict'  // Additional security measure
-  }
-  
+    cookie:{
+        expires:Date.now()+7*24*60*60*1000,
+        maxAge:7*24*60*60*1000,
+        httpOnly:true, 
+    }
 }
 
 // app.get("/",(req,res)=>{
